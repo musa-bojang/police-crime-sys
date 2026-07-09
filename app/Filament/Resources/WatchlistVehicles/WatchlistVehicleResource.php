b7d4c9b31a9254b3f36280050ff671569cb039f7
musa-bojang <musabojang413@gmail.com>
2026-07-09 21:13:52 +0100

    Stamp the creator automatically

--- app/Filament/Resources/WatchlistVehicles/WatchlistVehicleResource.php
+++ app/Filament/Resources/WatchlistVehicles/WatchlistVehicleResource.php
@@ -22,7 +22,7 @@ class WatchlistVehicleResource extends Resource
 {
     protected static ?string $model = WatchlistVehicle::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    //protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
     protected static ?string $recordTitleAttribute = 'plate';
 
